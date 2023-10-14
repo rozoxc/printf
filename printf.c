1ce2713a818f81250525e93ed15c4c3c531b78d2
a@fma.com <you@email.com>
2023-10-14 16:41:46 -0700

    ls

--- printf.c
+++ printf.c
@@ -4,46 +4,63 @@
  * @format:  is a character string.
  * Return: return the number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int chars_printed;
-	char *str;
+	char buffer[1024], *str, c;
+	int i, j, chars_printed;
 
-	chars_printed = 0;
 	va_start(args, format);
+	chars_printed = 0;
+	i = 0;
+	j = 0;
 	while (*format != '\0')
 	{
-		if (*format == '%')
+		if (*format != '%')
 		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					chars_printed += _putchar((char)va_arg(args, int));
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					while (*str != '\0')
-					{
-						chars_printed += _putchar(*str);
-						str++;
-					}
-					break;
-				case '%':
-					chars_printed += _putchar('%');
-					break;
-				default:
-					break;
-			}
-			format++;
+			buffer[i++] = *format;
+			chars_printed++;
 		}
 		else
 		{
-			chars_printed += _putchar(*format);
-			format++;
-		}	}
+			switch (*++format)
+			{
+				case 'c':
+					{
+						c = (char)va_arg(args, int);
+						buffer[i++] = c;
+						chars_printed++;
+					}
+					break;
+				case 's':
+					{
+						str = va_arg(args, char *);
+				while (*str != '\0')
+				{
+							buffer[i++] = *str;
+							str++;
+							chars_printed++
+						}
+					}
+					break;
+				case '%':
+					buffer[i++] = '%';
+					chars_printed++;
+					break;
+				default:
+					buffer[i++] = '%';
+					buffer[i++] = *format;
+					chars_printed += 2;
+					break;
+			}
+		}
+		format++;
+	}
+	while (j < i)
+	{
+		_putchar(buffer[j]);
+		j++;
+	}
 	va_end(args);
 	return (chars_printed);
 }
