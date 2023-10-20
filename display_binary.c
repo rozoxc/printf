60e534f06b2e58de71e008af92a6a0244cff04be
Abdelhadi Chajia <abdelhadi-chajia@outlook.fr>
2023-10-19 17:26:40 -0700

    updatings

--- display_binary.c
+++ display_binary.c
@@ -8,31 +8,37 @@
 **/
 int display_binary(va_list args)
 {
-	unsigned int num = va_arg(args, unsigned int);
-	int size = 0;
-	unsigned int bits[sizeof(unsigned int) * 8];
-	unsigned int i;
+	int decimal_number = va_arg(args, int);
+	int binary[32];
+	int i = 0;
+	int printed_chars = 0;
+	int j;
 
-	if (num == 0)
+	if (decimal_number < 0)
+	{
+		decimal_number = -decimal_number;
+		_putchar('-');
+		printed_chars++;
+	}
+
+	if (decimal_number == 0)
 	{
 		_putchar('0');
-		size++;
+		printed_chars++;
 	}
 	else
 	{
-		for (i = 0; i < sizeof(unsigned int) * 8; i++)
+		while (decimal_number > 0)
 		{
-			bits[i] = (num >> i) & 1;
+			binary[i] = decimal_number % 2;
+			decimal_number = decimal_number / 2;
+			i++;
 		}
-		i = sizeof(unsigned int) * 8 - 1;
-		while (i > 0 && bits[i] == 0)
-			i--;
-		for (; i > 0; i--)
+		for (j = i - 1; j >= 0; j--)
 		{
-			_putchar(bits[i] + '0');
-			size++;
+			_putchar('0' + binary[j]);
+			printed_chars++;
 		}
-		_putchar(bits[i] + '0');
 	}
-	return (size);
+	return (printed_chars);
 }
